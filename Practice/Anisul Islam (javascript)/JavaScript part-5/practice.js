332eb5b7ce14477f7ed5a3325505fd2733884b62
AshrafUzMahim <ashraf.uzzaman04082004@gmail.com>
2022-08-14 20:56:05 +0600

    yo you commit

---
+++ Practice/Anisul Islam (javascript)/JavaScript part-5/practice.js
@@ -0,0 +1,21 @@
+// ---------------------------
+alert("hahahah");
+
+let num = 20;
+num = toString(num);
+
+console.log(typeof num);
+
+// ---------------------------
+
+let number = "555";
+number = parseInt(number);
+
+console.log(typeof number);
+
+// ------------
+
+number = "2.55";
+number = parseFloat(number);
+
+console.log(typeof number);
