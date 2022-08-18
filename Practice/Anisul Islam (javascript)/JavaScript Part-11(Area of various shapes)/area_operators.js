35d5ab290dd3b4a474aa989c9e89e0870bdcf333
AshrafUzMahim <ashraf.uzzaman04082004@gmail.com>
2022-08-18 21:17:41 +0600

    js commit

---
+++ Practice/Anisul Islam (javascript)/JavaScript Part-11(Area of various shapes)/area_operators.js
@@ -0,0 +1,10 @@
+var base = parseFloat(prompt("Enter base ="));
+var height = parseFloat(prompt("Enter height ="));
+
+var area = base * height;
+
+document.write("Rectangle Area =" + area + "<br/>");
+
+var area2 = (base * height) / 2;
+
+document.write("Tirangle Area =" + area2);
